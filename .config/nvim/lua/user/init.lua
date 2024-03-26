ccc69b682ccb318a9faf46897c6460a35fa3f8b3
Rail Aliiev <rail@iqchoice.com>
2024-03-26 00:22:40 -0400

    add crlfmt

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -10,20 +10,18 @@ local lsp = {
   settings = {
     gopls = {
       env = {
-        GOPACKAGESDRIVER = './build/bazelutil/gopackagesdriver.sh',
+        GOPACKAGESDRIVER = "./build/bazelutil/gopackagesdriver.sh",
       },
       directoryFilters = {
         "-_bazel",
         "-inflight_trace_dump",
-      }
-    }
-  }
+      },
+    },
+  },
 }
 
 local plugins = {
-  { "AstroNvim/astrocommunity",
-    { import = "astrocommunity.pack.go" },
-  },
+  { "AstroNvim/astrocommunity", { import = "astrocommunity.pack.go" } },
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -60,36 +58,46 @@ local plugins = {
     opts = {},
   },
   {
-    "ray-x/go.nvim",
-    config = function()
-      require("go").setup()
-    end,
-    event = {"CmdlineEnter"},
-    ft = {"go", 'gomod'},
-    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    "mhartington/formatter.nvim",
+    ft = "go",
+    opts = {
+      filetype = {
+        go = {
+          function()
+            return {
+              exe = "crlfmt",
+              stdin = true,
+            }
+          end,
+        },
+      },
+    },
   },
 }
 
 local remember_last_position = function()
-  vim.api.nvim_create_autocmd(
-    { "BufWinEnter" },
-    {
-      callback = function(opts)
-        local ft = vim.bo[opts.buf].filetype
-        if ft:match('commit') or ft:match('rebase') then
-          return
-        end
-        local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
-        if { row, col } ~= { 0, 0 } then
-          vim.api.nvim_win_set_cursor(0, { row, col })
-        end
-      end,
-    }
-  )
+  vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+    callback = function(opts)
+      local ft = vim.bo[opts.buf].filetype
+      if ft:match "commit" or ft:match "rebase" then return end
+      local row, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+      if { row, col } ~= { 0, 0 } then vim.api.nvim_win_set_cursor(0, { row, col }) end
+    end,
+  })
+end
+
+local crlfmt_save = function()
+  vim.api.nvim_create_augroup("__formatter__", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.go",
+    group = "__formatter__",
+    command = ":FormatWrite",
+  })
 end
 
 local polish = function()
   remember_last_position()
+  crlfmt_save()
 end
 
 return {
