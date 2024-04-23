01c550830ed7b523266594021646deca8bbc1bbf
Rail Aliiev <rail@iqchoice.com>
2024-04-23 14:41:57 -0400

    use custom mason repo for crlfmt

--- .config/nvim/lua/plugins/crlfmt.lua
+++ .config/nvim/lua/plugins/crlfmt.lua
@@ -1,12 +1,25 @@
 ---@type LazySpec
 return {
-  -- {
-  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "crlfmt" })
-  --   end,
-  -- },
+  {
+    "williamboman/mason.nvim",
+    --[[
+      yq ea '[.]' -o json packages/crlfmt/package.yaml > registry.json
+      zip -r registry.json.zip registry.json
+      sha256sum registry.json registry.json.zip > checksums.txt
+      attach to the release
+    --]]
+    opts = function(_, opts)
+      opts.registries =
+        require("astrocore").list_insert_unique(opts.registries, { "github:rail/mason-registry@crlfmt" })
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "crlfmt" })
+    end,
+  },
   {
     "stevearc/conform.nvim",
     opts = {
